d0181e495ecb4e67101a6bbf2a08a06acb8bbbdf
vagrant <vagrant@vagrant.vm>
2019-04-05 05:26:29 +0000

    removed some stuff

--- soup.py
+++ soup.py
@@ -14,30 +14,18 @@ def get_name(team):
 def get_position(team):
  return team.find(class_='team-position ml2 pr3').text
 
-def get_table(teams):
+def get_teams_as_dictionary(teams):
   names = map(get_name, teams)
   positions = map(get_position, teams)
-  return { positions: positions, names: names }
-
-def get_teams_as_dictionary(teams):
-  names = list(map(get_name, teams))
-  positions = map(get_position, teams)
   teamsDict = {}
+  i = 0
   for name, position in zip(names, positions):
-    teamsDict[name] = { 'position': position }
+    teamsDict[i] = { 'team': name, 'position': position }
+    i += 1
   return teamsDict
 
 teams = soup.find_all(class_='team-link flex items-center clr-gray-03')
-
-titles = list(map(get_name, teams))
-positions = map(get_position, teams)
 teamsPT = get_table(teams)
-
 teamsDict = get_teams_as_dictionary(teams)
 
 print(teamsDict)
-
-#for title, position in zip(titles, positions):
-  #print('title:', title, '\nposition:', position)
-
-#print(teamsPT)
