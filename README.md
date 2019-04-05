c3c910314362ec45d0009dc706e9290efe01c43e
vagrant <vagrant@vagrant.vm>
2019-04-05 04:47:52 +0000

    futbol api init

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a futbol API
+
+I am creating this api because most of the soccer apis are either expensive or
+only offer a few requests per day/hour.
+
+How am I achieving this, simple, by scrapping through ESPN's website, getting the scores, standings, stats, etc
+and then creating a REST api to access the data.
---
+++ soup.py
@@ -0,0 +1,36 @@
+#!usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.espn.com/soccer/table/_/league/mex.1'
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+def get_name(team):
+  return team.a.img.get('title')
+
+def get_position(team):
+ return team.find(class_='team-position ml2 pr3').text
+
+def get_table(teams):
+  names = map(get_name, teams)
+  positions = map(get_position, teams)
+  return { positions: positions, names: names }
+
+def get_teams_as_dictionary(teams):
+  
+
+teams = soup.find_all(class_='team-link flex items-center clr-gray-03')
+
+titles = list(map(get_name, teams))
+positions = map(get_position, teams)
+teamsPT = get_table(teams)
+
+print(titles)
+
+for title, position in zip(titles, positions):
+  print('title:', title, '\nposition:', position)
+
+#print(teamsPT)
